21d9570a13ae69e3731503b966737eb233854304
felix6464 <felix.boette@student.uni-tuebingen.de>
2023-06-16 12:07:06 +0200

    added __main__.py

---
+++ LIM/neural_networks/__main__.py
@@ -0,0 +1,10 @@
+# Import necessary modules or classes
+import lstm_training as lstm
+
+
+
+# Check if the module is run as a script
+if __name__ == "__main__":
+
+    # Call the main function
+    lstm.main()
